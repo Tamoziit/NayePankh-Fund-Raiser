9f113ddb8bc38a1fbe169d29760ad8470628ea64
Tamojit Das <tamojitdas181007@gmail.com>
2024-10-16 23:04:03 +0530

    Deployment micro-opti: 2

--- backend/services/paypal.js
+++ backend/services/paypal.js
@@ -50,8 +50,8 @@ const createOrder = async () => {
             }
         }],
         application_context: {
-            return_url: `http://localhost:5000/complete-order/${encodeURIComponent(latestEntry.name)}/${encodeURIComponent(latestEntry.email)}/${encodeURIComponent(latestEntry.mobileNo)}/${encodeURIComponent(latestEntry.referenceCode)}/${encodeURIComponent(latestEntry.price)}`,
-            cancel_url: "http://localhost:5000/cancel-order",
+            return_url: `${process.env.VITE_APP_URL}/complete-order/${encodeURIComponent(latestEntry.name)}/${encodeURIComponent(latestEntry.email)}/${encodeURIComponent(latestEntry.mobileNo)}/${encodeURIComponent(latestEntry.referenceCode)}/${encodeURIComponent(latestEntry.price)}`,
+            cancel_url: `${process.env.VITE_APP_URL}/cancel-order`,
             user_action: "PAY_NOW",
             brand_name: ""
         }
